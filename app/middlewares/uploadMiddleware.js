e229df071acc325bdd9d829f58215918dc31046b
Chris Berlant <chris.berlant@gmail.com>
2023-10-02 11:51:34 +0200

    updated upload middleware

--- app/middlewares/uploadMiddleware.js
+++ app/middlewares/uploadMiddleware.js
@@ -4,17 +4,17 @@ const storage = multer.diskStorage({
   destination: function(req, file, cb) {    // Destination folder
     return cb(null, "./uploads");
   },
-  filename: function(req, file, cb) {       // Set the title of the stored file using timestamp and title provided by the user
+  filename: function(req, file, cb) {       // Set the title of the stored file using timestamp and original title of the document
     return cb(null, `${Date.now()}_${file.originalname}`)
   }
 });
 
 const fileFilter = (req, file, cb) => {
   // Only accept these type of files
-  const allowedFileExtensions = ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx','odt', 'ods', 'odp', 'ott', 'ots', 'otp','jpg', 'jpeg', 'png', 'gif'];
+  const allowedFileExtensions = ['txt', 'pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx','odt', 'ods', 'odp', 'ott', 'ots', 'otp','jpg', 'jpeg', 'png', 'gif'];
 
   // Get the file extension from the name
-  const fileExtension = file.originalname.split('.').pop();
+  const fileExtension = file.originalname.split('.').pop().toLowerCase();
   
   // Check if this extension is allowed
   if (allowedFileExtensions.includes(fileExtension)) {
