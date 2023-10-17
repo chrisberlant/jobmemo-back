6507f636f8967eefddf8fbe0f40a3571e38dd734
Chris Berlant <chris.berlant@gmail.com>
2023-10-17 23:08:07 +0200

    updated comments, cleared uploads folder

--- app/middlewares/requestsLimitMiddleware.js
+++ app/middlewares/requestsLimitMiddleware.js
@@ -1,10 +1,10 @@
 import { rateLimit } from 'express-rate-limit';
 
 const requestsLimitMiddleware = rateLimit({
-    windowMs: 60 * 1000,
-    max: 5,
+    windowMs: 60 * 1000, // In one minute
+    max: 5, // Maximal queries allowed
     message: "Nombre maximal de requêtes par minute atteint.",
     headers: true,
   });
 
-  export default requestsLimitMiddleware;
+export default requestsLimitMiddleware;
